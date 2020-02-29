e6e7a2675b2420429801924ebc1b4b5bb4a3053d
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2020-02-29 16:45:28 +0800

    stateless & stateful

---
+++ stateful_stateless_func.js
@@ -0,0 +1,11 @@
+// var React = require('react');
+var ReactDOM = require('react-dom');
+
+// (p)=>{} is a function in JS
+// below is a stateless func
+const sum = (a, b, c) => a + b + c;
+
+let a = 0;
+const sum = () => a++;
+
+ReactDOM.render();
